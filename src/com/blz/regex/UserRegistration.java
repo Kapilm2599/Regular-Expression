55fb931f85d5c1b77d52c6410ce2af1e85431182
Kapilm2599 <mahajankapil575@gmail.com>
2023-04-27 16:06:41 +0530

    Added: To validation of last name

--- src/com/blz/regex/UserRegistration.java
+++ src/com/blz/regex/UserRegistration.java
@@ -12,11 +12,19 @@ public class UserRegistration {
 	public static void main(String[] args) {
 		System.out.println("welcome to User Registration program");
 		Scanner scanner = new Scanner(System.in);
+		
 		System.out.println("Enter First Name: ");
 		String firstName = "[A-Z]{1}[a-z]{2,}";
 		String input = scanner.next(); 
+		
+		System.out.println("Enter Last Name: ");
+		String lastName = "[A-z]{1}[a-z]{2,}";
+		String inputLastName = scanner.next();
+		
 		UserRegistration userRegistration = new UserRegistration();
 		System.out.println(userRegistration.userName(firstName, input));
+		System.out.println(userRegistration.userName(lastName, inputLastName));
+
 	}
 
 }
