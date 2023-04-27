e379e65c20ba7f92bad5d4272343f452f482eb42
Kapilm2599 <mahajankapil575@gmail.com>
2023-04-27 16:02:50 +0530

    Added: To validation of first name

---
+++ src/com/blz/regex/UserRegistration.java
@@ -0,0 +1,22 @@
+package com.blz.regex;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+	
+	public boolean userName(String regex, String input) {
+		return Pattern.compile(regex).matcher(input).matches();
+	}
+
+	public static void main(String[] args) {
+		System.out.println("welcome to User Registration program");
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter First Name: ");
+		String firstName = "[A-Z]{1}[a-z]{2,}";
+		String input = scanner.next(); 
+		UserRegistration userRegistration = new UserRegistration();
+		System.out.println(userRegistration.userName(firstName, input));
+	}
+
+}
